2bb1a446c6a3b00d2543189c968a73ada1c72f54
ADsglt77 <adrien.segalat@gmail.com>
2026-02-04 18:41:14 +0100

    🗄️ - (db) Add type field to timeline items

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -3,6 +3,7 @@ export interface TimelineItem {
   organization: string
   role?: string
   description?: string
+  type: 'formation' | 'work'
 }
 
 export const timelineData: TimelineItem[] = [
@@ -11,11 +12,13 @@ export const timelineData: TimelineItem[] = [
     organization: 'Lycée Turgot (Limoges)',
     role: 'Seconde (option SI-CIT), Première (Maths/NSI/Physique-Chimie), Terminale (Maths/NSI)',
     description: 'Baccalauréat général mention Très Bien',
+    type: 'formation',
   },
   {
     date: 'Mars 2020',
     organization: 'Police Nationale',
     role: 'Stage de trois jours',
+    type: 'work',
   },
   {
     date: 'Juin 2021',
@@ -23,28 +26,33 @@ export const timelineData: TimelineItem[] = [
     role: "Stage d'une semaine",
     description:
       'Photogravure, découverte métier + journée avec gestionnaire informatique + responsable site web',
+    type: 'work',
   },
   {
     date: '2023/2024',
     organization: 'Lycée Valadon (Limoges)',
     role: 'BTS SIO (Services Informatiques aux Organisations)',
     description: 'Option "Certification"',
+    type: 'formation',
   },
   {
     date: 'Mai/Juin 2024',
     organization: 'Broussaud Textile',
     role: 'Stage de cinq semaines',
     description: 'Symfony/PHP, migration API REST vers GraphQL, découverte rôle dev en entreprise',
+    type: 'work',
   },
   {
     date: 'Depuis Mai 2024',
     organization: 'Super U (Aixe-Sur-Vienne / Limoges)',
     role: 'Job étudiant (Grande surface)',
+    type: 'work',
   },
   {
     date: 'Janvier/Février 2025',
     organization: 'Condat Basket Club',
     role: 'Stage de sept semaines',
     description: 'Site vitrine + dashboard, NuxtJS/Vue/TypeScript/Prisma',
+    type: 'work',
   },
 ]
